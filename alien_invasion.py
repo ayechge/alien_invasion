a3960b8911cc9e2608a11749ff1bbd712fd24f6c
zhaoty <ayechge@outlook>
2019-06-22 21:08:14 +0800

    发射子弹 删除屏幕外子弹 代码重构

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 import game_functions as gf
 from settings import Settings
 from ship import Ship
-
+from pygame.sprite import Group
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象,设置屏幕对象
@@ -14,12 +14,15 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings,screen)
+    #创建存储子弹的编组
+    bullets=Group()
     # 开始游戏主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
+        gf.update_bullets(bullets)
         # 更新屏幕图像，并切换到新图像
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship,bullets)
 
 run_game()
